8db76a164139c2b88176b5702b643abefb390058
bitusr <dm.kolotov@mail.ru>
2021-02-23 11:23:04 +0100

    Create simple express app with the connection to mongo

---
+++ mongo/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const { MongoClient } = require('mongodb')
+
+// normally you would do:
+// const connectionString = process.env.MONGO_WHATEVER_VAR
+const connectionString = `mongodb://localhost:27017`
+
+async function init() {
+  const client = new MongoClient(connectionString, {
+    useUnifiedTopology: true, // ask Mongo to use new way of discovering servers
+  })
+  await client.connect() // connect to mongo from your node server
+
+  const app = express()
+
+  app.get('/get', async (req, res) => {
+    const db = await client.db('adoption')
+    const collection = db.collection('pets')
+
+    const pets = await collection
+      .find(
+        { $text: { $search: req.query.search } },
+        { _id: 0 }
+      )
+      .sort({ score: { $meta: 'textScore' }})
+      .limit(10)
+      .toArray()
+
+    res.json({ status: 'ok', pets }).end()
+  })
+
+  const PORT = 3000
+
+  app.use(express.static('./static'))
+  app.listen(PORT)
+  console.log(`Running on http://localhost:${PORT}`)
+}
+
+init()
